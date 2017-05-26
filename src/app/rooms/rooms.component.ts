1209de690a58688e593af2e01313b29fd0d09cb6
Elanna Grossman <elanna.grossman@gmail.com>
2017-05-25 20:49:41 -0700

    feat(rooms): add `activatedRoute` to constructor

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls   : ['./rooms.component.css'],
 })
 export class RoomsComponent implements OnInit {
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
