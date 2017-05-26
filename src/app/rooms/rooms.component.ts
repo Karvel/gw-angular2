29fd0bfe3ed3178c82730cf023166c1674917320
Elanna Grossman <elanna.grossman@gmail.com>
2017-05-25 20:48:43 -0700

    style(rooms): fix linter issues

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-rooms',
-  templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  selector    : 'app-rooms',
+  templateUrl : './rooms.component.html',
+  styleUrls   : ['./rooms.component.css'],
 })
 export class RoomsComponent implements OnInit {
 
